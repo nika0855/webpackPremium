b8b7a4134ad9a1cf2e4d19fadc468b8bda9a8e18
nika0855 <esaul555@mail.ru>
2020-01-08 08:12:25 +0300

    Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello webpack');
+
+import AppService from './modules/app.service'
+import {config} from './modules/config'
+import './modules/header.component'
+console.log('Config key:', config.key);
+
+const service = new AppService('Hello webpack')
+service.log()
